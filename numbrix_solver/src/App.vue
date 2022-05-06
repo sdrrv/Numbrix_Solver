286a5459b2a4f82d1c9b38aee0f319505835d470
sdrrv <duartecelvas@gmail.com>
2022-05-06 16:23:31 +0100

    Mini grid created

--- numbrix_solver/src/App.vue
+++ numbrix_solver/src/App.vue
@@ -1,56 +1,27 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
+  <v-app style="background-color: dimgray">
     <v-main>
-      <HelloWorld/>
+      <v-row class="text-center my-2">
+        <v-col cols="12">
+          <h1 class="white--text" style="font-family: 'Roboto', sans-serif ">Numbrix Solver</h1>
+        </v-col>
+        <v-col>
+          <board-view/>
+        </v-col>
+      </v-row>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+
+import boardView from "@/components/boardView";
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    boardView
   },
 
   data: () => ({
---
+++ numbrix_solver/src/components/boardView.vue
@@ -0,0 +1,50 @@
+<template>
+
+  <div class = "board">
+    <div class="box">X</div>
+    <div class="box">O</div>
+    <div class="box">O</div>
+    <div class="box">O</div>
+    <div class="box">X</div>
+    <div class="box">O</div>
+    <div class="box">O</div>
+    <div class="box">X</div>
+    <div class="box">X</div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "boardView"
+}
+</script>
+
+
+
+
+<style scoped>
+.board{
+  width: 900px;
+  height: 550px;
+  margin: 0 auto;
+  background-color: #555555;
+  color: #fff;
+  border: 6px solid #404040;
+  border-radius: 10px;
+  display: grid;
+  grid-template: repeat(3, 1fr) / repeat(3, 1fr);
+}
+
+.box
+{
+  border: 6px solid #404040;
+  font-family: Helvetica,serif;
+  font-weight: bold;
+  font-size: 4em;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+</style>
