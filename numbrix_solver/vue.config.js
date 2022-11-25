0214ce119d12919f3edcee5840c60a8d719c5b85
sdrrv <duartecelvas@gmail.com>
2022-11-25 13:13:54 +0000

    Deploy fix

--- numbrix_solver/vue.config.js
+++ numbrix_solver/vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "/numbrix-solver/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/Numbrix_Solver/" : "/",
   transpileDependencies: [
     'vuetify'
   ]
